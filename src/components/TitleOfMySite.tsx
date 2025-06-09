d7a61ff222e3e43fca37985acf2059863e524d6d
purple <purple@purples-iMac.local>
2025-06-09 12:55:19 -0400

    pink font

---
+++ src/components/TitleOfMySite.tsx
@@ -0,0 +1,11 @@
+const myName = "xtine"
+
+const TitleOfMySite = () => {
+   return (
+   <div>
+    <h1 className="font-black text-7xl">{myName} quiz</h1>
+    </div>
+  );
+}
+
+export default TitleOfMySite
